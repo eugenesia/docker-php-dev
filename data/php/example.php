e57c7baa87cfe11f83e172776a8aaf719c6b88d8
Eugene Sia <eugene@eugenesia.co.uk>
2017-07-22 18:17:56 +0100

    Connect to database in example.php

--- data/php/example.php
+++ data/php/example.php
@@ -3,3 +3,23 @@
 <p>This is PHP version <?php print phpversion(); ?>
   on port <?php print $_SERVER['SERVER_PORT']; ?></p>
 
+<?php
+  // Connect to MySQL server on container "mysql" (defined as service name in
+  // docker-compose.yml). Credentials are configured in docker-compose.yml.
+  $db_host = 'mysql';
+  $db_user = 'root';
+  $db_pass = 'root';
+  $db_name = 'php';
+  $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+  // Create a table and show it.
+  $result = $conn->query('CREATE TABLE IF NOT EXISTS tbl1');
+  $result = $conn->query('SHOW TABLES');
+
+  if (! $result) {
+    print 'MySQL query failed.';
+  }
+  else {
+    print_r($result->fetch_assoc());
+  }
+
